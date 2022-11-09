3b1db2f18809f4736dacb39c7f3e2d02a07371eb
Rafael-Carballo <rafael.carballo@potros.itson.edu.mx>
2022-11-08 23:26:36 -0700

    Terminado el Extra y el 6 del sprint 2
    se terminaron los criterios del extra y se completo la historia 6 del sprint 2

--- src/frames/FmrPomodoro.java
+++ src/frames/FmrPomodoro.java
@@ -18,23 +18,29 @@ import objeto.Tarea;
 public class FmrPomodoro extends javax.swing.JFrame {
 
     private Timer timer;
+    private Timer timerBlink;
     private int numPomodoro;
     private boolean pausa;
     private Tarea haciendo;
-    private final String leyenda="Haciendo: ";
+    private final String leyenda = "Haciendo: ";
+    private final String leyendaCont = "Faltan:";
     private final String pomodoroTime = "0:00:15";
     private final String descansoCorto = "0:00:07";
     private final String descansoLargo = "0:00:10";
+
     /**
      * Creates new form FmrPomodoro
+     *
      * @param tarea
      */
     public FmrPomodoro(Tarea tarea) {
-        this.haciendo=tarea;
+
+        this.haciendo = tarea;
         initComponents();
-        this.jLabel3.setText(leyenda+haciendo.getNombre());
+        this.jLabel3.setText(leyenda + haciendo.getNombre());
         btnRestart.setEnabled(false);
         btnPausa.setEnabled(false);
+        jLabel4.setVisible(false);
     }
 
     /**
@@ -54,6 +60,7 @@ public class FmrPomodoro extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -97,6 +104,8 @@ public class FmrPomodoro extends javax.swing.JFrame {
             }
         });
 
+        jLabel4.setText("jLabel4");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -110,12 +119,18 @@ public class FmrPomodoro extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 270, Short.MAX_VALUE)
                         .addContainerGap(45, Short.MAX_VALUE))))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(211, 211, 211)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(201, 201, 201)
+                        .addComponent(jLabel4)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 331, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(71, 71, 71))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(btnIniciar)
                         .addGap(69, 69, 69)
@@ -125,11 +140,10 @@ public class FmrPomodoro extends javax.swing.JFrame {
                                 .addComponent(btnPausa)
                                 .addGap(40, 40, 40)
                                 .addComponent(btnRestart)))
-                        .addGap(90, 90, 90))))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(211, 211, 211)
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(90, 90, 90))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 331, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(53, 53, 53))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -140,6 +154,8 @@ public class FmrPomodoro extends javax.swing.JFrame {
                 .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(lbTiempo, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel4)
                 .addGap(18, 18, 18)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
@@ -149,7 +165,7 @@ public class FmrPomodoro extends javax.swing.JFrame {
                     .addComponent(btnRestart))
                 .addGap(18, 18, 18)
                 .addComponent(jButton1)
-                .addContainerGap(51, Short.MAX_VALUE))
+                .addGap(34, 34, 34))
         );
 
         pack();
@@ -169,32 +185,39 @@ public class FmrPomodoro extends javax.swing.JFrame {
         if (!pausa) {
             pausa = true;
             this.btnPausa.setText("Reanudar");
+            
         } else {
             pausa = false;
             btnPausa.setText("Pause");
             btnRestart.setEnabled(true);
             this.btnPausa.setText("Pausar");
+            
         }
+        blink();
     }//GEN-LAST:event_btnPausaActionPerformed
 
     private void btnRestartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRestartActionPerformed
         int opcion = JOptionPane.showConfirmDialog(this, "Seguro de querer reiniciar?", "Confirmacion", JOptionPane.OK_CANCEL_OPTION,
                 JOptionPane.QUESTION_MESSAGE);
         if (opcion == JOptionPane.OK_OPTION) {
-        timer.cancel();
-        lbTiempo.setText("00:00:00");
-        btnPausa.setEnabled(false);
-        btnRestart.setEnabled(false);
-        btnIniciar.setEnabled(true);
-        jButton1.setEnabled(true);
-        this.btnPausa.setText("Pausar");
+            timer.cancel();
+            lbTiempo.setText("00:00:00");
+            btnPausa.setEnabled(false);
+            btnRestart.setEnabled(false);
+            btnIniciar.setEnabled(true);
+            jButton1.setEnabled(true);
+            this.btnPausa.setText("Pausar");
+            jLabel4.setVisible(false);
+            numPomodoro=1;
+            jLabel1.setText((4 - numPomodoro) != 0 ? "Descansos antes del descanso largo: " + (4 - numPomodoro) : "El siguiente es un descanso largo!!");
         }
     }//GEN-LAST:event_btnRestartActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        fmrTareasPendientes frm=new fmrTareasPendientes();
+        fmrTareasPendientes frm = new fmrTareasPendientes();
         frm.setVisible(true);
         this.dispose();
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     public void setTime(int time) {
@@ -204,9 +227,29 @@ public class FmrPomodoro extends javax.swing.JFrame {
         int hour = (timeNow / (1000 * 60 * 60)) % 24;
         lbTiempo.setText(String.format("%02d:%02d:%02d", hour, minute, second));
     }
-
-    private void pomodoroTimer() {
+    private void blink(){
         
+        if (!pausa) {
+            timerBlink.cancel();
+        }else{
+            
+        timerBlink=new Timer();
+        timerBlink.schedule(new TimerTask(){
+            int counter=0;
+            @Override
+            public void run() {
+                counter++;
+                if (counter%2==0) {
+                    lbTiempo.setEnabled(false);
+                }else{
+                    lbTiempo.setEnabled(true);
+                }
+            }
+        }, 0,1000);
+        }
+    }
+    private void pomodoroTimer() {
+        jLabel4.setVisible(false);
         timer = new Timer();
         timer.schedule(new TimerTask() {
             int counter = 0;
@@ -220,23 +263,41 @@ public class FmrPomodoro extends javax.swing.JFrame {
                 if (!pausa) {
                     ++counter;
                     setTime(counter);
-                    if (counter==(cont-5)) {
+                    if (counter >= (cont - 5)) {
                         Toolkit.getDefaultToolkit().beep();
-                        JOptionPane.showMessageDialog(null, "5 segundos para el descanso");
+                        jLabel4.setVisible(true);
+                        jLabel4.setText(leyendaCont + " " + (cont - counter) + " segundos para el descanso");
                     }
-                    if (counter == cont && numPomodoro < 4) //if less than 4 pomodoros are complete, take a short break
-                    {
+                    if (counter == cont) {
+                        int opcion = JOptionPane.showConfirmDialog(null, numPomodoro < 4 ? "Completaste un pomodoro, Quieres omitir un pequeño descanso??" : "Completaste 4 pomodoros, Quieres omitir un gran descanso??", "Descanso!!", JOptionPane.YES_NO_OPTION,
+                                JOptionPane.QUESTION_MESSAGE);
+                        jLabel4.setVisible(false);
                         timer.cancel();
-                        Toolkit.getDefaultToolkit().beep();
-                        JOptionPane.showMessageDialog(null, "Completaste un pomodoro, Puedes tomar un pequeño descanso!!");
-                        descansoCorto();
-                        
-                    } else if (counter == cont && numPomodoro == 4) //if 4 pomodoros are complete, take a long break
-                    {
-                        timer.cancel();
-                        Toolkit.getDefaultToolkit().beep();
-                        JOptionPane.showMessageDialog(null, "Completaste 4 pomodoros, Puedes tomar un gran descanso!!");
-                        descansoLargo();
+                        int confirm=JOptionPane.NO_OPTION;
+                        if (opcion==JOptionPane.YES_OPTION) {
+                            confirm = JOptionPane.showConfirmDialog(null, "Seguro que deseas omitir?", "Confirmacion!!", JOptionPane.YES_NO_OPTION,
+                                JOptionPane.QUESTION_MESSAGE);
+                        }
+                        if (confirm == JOptionPane.NO_OPTION) {
+                            
+                            if (numPomodoro < 4) //if less than 4 pomodoros are complete, take a short break
+                            {
+                                descansoCorto();
+
+                            } else if (numPomodoro == 4) //if 4 pomodoros are complete, take a long break
+                            {
+                                descansoLargo();
+                            }
+                        } else {
+                            
+                            if (numPomodoro == 4) {
+                                numPomodoro = 1;
+                            }else{
+                            numPomodoro++;
+                            }
+                            jLabel1.setText((4 - numPomodoro) != 0 ? "Descansos antes del descanso largo: " + (4 - numPomodoro) : "El siguiente es un descanso largo!!");
+                            pomodoroTimer();
+                        }
                     }
                 }
             }
@@ -245,7 +306,7 @@ public class FmrPomodoro extends javax.swing.JFrame {
     }
 
     private void descansoCorto() {
-        
+        jLabel4.setVisible(false);
         timer = new Timer();
         timer.schedule(new TimerTask() {
             int cont = 0;
@@ -258,17 +319,18 @@ public class FmrPomodoro extends javax.swing.JFrame {
                 if (!pausa) {
                     ++cont;
                     setTime(cont);
-                    if (cont==(lim-5)) {
+                    if (cont >= (lim - 5)) {
                         Toolkit.getDefaultToolkit().beep();
-                        JOptionPane.showMessageDialog(null, "5 segundos para empezar de nuevo");
+                        jLabel4.setVisible(true);
+                        jLabel4.setText(leyendaCont + " " + (lim - cont) + " segundos para el descanso");
                     }
                     if (cont == lim) {
                         timer.cancel();
                         Toolkit.getDefaultToolkit().beep();
                         JOptionPane.showMessageDialog(null, "Descanso corto finalizado");
                         numPomodoro++;
-                        int descansosFaltantes=4-numPomodoro;
-                        jLabel1.setText(descansosFaltantes!=0?"Descansos antes del descanso largo: "+(4-numPomodoro):"El siguiente es un descanso largo!!");
+                        int descansosFaltantes = 4 - numPomodoro;
+                        jLabel1.setText(descansosFaltantes != 0 ? "Descansos antes del descanso largo: " + (4 - numPomodoro) : "El siguiente es un descanso largo!!");
                         pomodoroTimer();
 
                     }
@@ -278,7 +340,7 @@ public class FmrPomodoro extends javax.swing.JFrame {
     }
 
     private void descansoLargo() {
-        
+        jLabel4.setVisible(false);
         jLabel1.setText("Descanso largo!!! ");
         timer = new Timer();
         timer.schedule(new TimerTask() {
@@ -292,16 +354,17 @@ public class FmrPomodoro extends javax.swing.JFrame {
                 if (!pausa) {
                     ++cont;
                     setTime(cont);
-                    if (cont==(counterLimit-5)) {
+                    if (cont >= (counterLimit - 5)) {
                         Toolkit.getDefaultToolkit().beep();
-                        JOptionPane.showMessageDialog(null, "5 segundos para empezar de nuevo");
+                        jLabel4.setVisible(true);
+                        jLabel4.setText(leyendaCont + " " + (counterLimit - cont) + " segundos para el descanso");
                     }
                     if (cont == counterLimit) {
                         timer.cancel();
                         Toolkit.getDefaultToolkit().beep();
                         JOptionPane.showMessageDialog(null, "Es hora de continuar con las tareas.");
                         numPomodoro = 1;
-                        jLabel1.setText("Descansos antes del descanso largo: "+(4-numPomodoro));
+                        jLabel1.setText("Descansos antes del descanso largo: " + (4 - numPomodoro));
                         pomodoroTimer();
                     }
                 }
@@ -318,6 +381,7 @@ public class FmrPomodoro extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel lbTiempo;
     // End of variables declaration//GEN-END:variables
 }
